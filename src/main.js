0eba3c70b065a3802b218137648b317f1c1aba4a
wangkangzhang <1368504885@qq.com>
2021-12-23 16:49:51 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import TDesign from 'tdesign-vue';
+// 引入组件库全局样式资源
+import 'tdesign-vue/es/style/index.css';
 
-Vue.config.productionTip = false
+Vue.use(TDesign);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,156 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <t-form
+        :data="formData"
+        :rules="rules"
+        ref="form"
+        @reset="onReset"
+        @submit="onSubmit"
+        scrollToFirstError="smooth"
+    >
+      <t-form-item label="用户名" help="这里请填写用户名" name='account'>
+        <t-input v-model="formData.account"></t-input>
+      </t-form-item>
+      <t-form-item label="密码" help="这里请填写密码" name='password'>
+        <t-input type="password" v-model="formData.password"></t-input>
+      </t-form-item>
+      <t-form-item label="邮箱" name='email' >
+        <t-input v-model="formData.email" :email="true"></t-input>
+      </t-form-item>
+      <t-form-item label="性别" name='gender'>
+        <t-radio-group v-model="formData.gender">
+          <t-radio value="male">男</t-radio>
+          <t-radio value="femal">女</t-radio>
+        </t-radio-group>
+      </t-form-item>
+      <t-form-item label="课程" name='course'>
+        <t-checkbox-group
+            v-model="formData.course"
+            :options="courseOptions"
+        ></t-checkbox-group>
+      </t-form-item>
+      <t-form-item label="学院" name='college'>
+        <t-select v-model="formData.college" class="demo-select-base" clearable>
+          <t-option v-for="(item, index) in options" :value="item.value" :label="item.label" :key="index">
+            {{ item.label }}
+          </t-option>
+        </t-select>
+      </t-form-item>
+      <t-form-item label="入学时间" name='date' :rules="[{ date: { delimiters: ['/', '-', '.'] }, message: '日期格式有误' }]">
+        <t-input v-model="formData.date"></t-input>
+      </t-form-item>
+      <t-form-item label="个人网站" name='content.url'>
+        <t-input v-model="formData.content.url"></t-input>
+      </t-form-item>
+      <t-form-item label="数字" name='number'>
+        <t-input disabled  v-model="formData.number"></t-input>
+      </t-form-item>
+      <t-input-number
+          v-model="value"
+          theme="normal"
+          :max="15"
+          :min="-2"
+      ></t-input-number>
+      <t-form-item style="padding-top: 8px">
+        <t-button theme="primary" type="submit" style="margin-right: 10px">提交</t-button>
+        <t-button theme="default" variant="base" type="reset" style="margin-right: 10px">重置</t-button>
+        <t-button theme="default" variant="base" @click="handleClear">清除校验结果</t-button>
+      </t-form-item>
+    </t-form>
+    <div class="tdesign-demo-item--datepicker">
+      <t-date-picker mode="month" format="YYYY-MM"></t-date-picker>
+    </div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  const INITIAL_DATA = {
+    account: '',
+    password: '',
+    email: '',
+    gender: '',
+    college: '',
+    date: '',
+    content: {
+      url: '',
+    },
+    number: null,
+    course: []
+  };
+  export default {
+    data() {
+      return {
+        value: 0,
+        formData: { ...INITIAL_DATA },
+        courseOptions: [
+          { label: '语文', value: '1' },
+          { label: '数学', value: '2' },
+          { label: '英语', value: '3' },
+          { label: '体育', value: '4' },
+        ],
+        options: [
+          { label: '计算机学院', value: '1' },
+          { label: '软件学院', value: '2' },
+          { label: '物联网学院', value: '3' },
+        ],
+        rules: {
+          account: [
+            { required: true, message: '姓名必填', type: 'error' },
+            { min: 2, message: '至少需要两个字', type: 'error' },
+          ],
+          password: [
+            { required: true, message: '密码必填', type: 'error' },
+          ],
+          email: [
+            { required: true, message: '格式必须为邮箱', type: 'warning' },
+            { email: true }
+          ],
+          number: [
+            { required: true, message: '格式必须数值', type: 'warning' },
+            { number: true, type: 'success' }
+          ],
+          gender: [
+            { required: true, message: '性别必填', type: 'warning' },
+          ],
+          course: [
+            { required: true, message: '课程必填', type: 'warning' },
+          ],
+          'content.url': [
+            { required: true, message: '个人网站必填', type: 'warning' },
+            {
+              url: {
+                protocols: ['http', 'https', 'ftp'],
+                require_protocol: true,
+              },
+              message: '请输入正确的个人主页',
+            },
+          ],
+        },
+      };
+    },
+
+    methods: {
+      onReset() {
+        this.$message.success('重置成功');
+        console.log('formData', this.formData);
+      },
+      onSubmit({ validateResult, firstError }) {
+        if (validateResult === true) {
+          this.$message.success('提交成功');
+        } else {
+          console.log('Errors: ', validateResult);
+          this.$message.warning(firstError);
+        }
+      },
+      handleClear() {
+        this.$refs.form.clearValidate();
+      },
+    },
+  };
 </script>
+
+<style scoped>
+  .demo-select-base {
+    width: 300px;
+  }
+</style>
